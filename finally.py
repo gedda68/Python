30bd2e75223cd27bd883f0f979043ec06f750c2e
gedda <gaitchis@gmail.com>
2021-09-10 08:28:38 +1000

    updated for Python 3

--- finally.py
+++ finally.py
@@ -1,7 +1,7 @@
 x = input("Enter a Number: ")
 y = 0
 try:
-	y = 10 / x
-	print "y =", y
+	y = 10 / int(x) * 100
+	print("y =", y)
 finally:
-	print "In Finally Y=", y
+	print("In Finally Y=", format(y, '.2f'), "%")
